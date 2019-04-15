46a4773f60c67871d323ad09732a3d75868eabaa
eduran84 <22216687+eduran84@users.noreply.github.com>
2019-04-15 17:34:58 +0200

    Fixed toggle button being initialized with red background.

--- ui/toggle_button.lua
+++ ui/toggle_button.lua
@@ -8,7 +8,7 @@ local gcTB = GC(NAME, {
   params = {
 		type = "sprite-button",
 		sprite = TB_WITHOUT_ALERT_SPRITE,
-    style = "ltnt_toggle_button_with_alert",
+    style = "icon_button",
 		tooltip = {"ltnt.main-button-tooltip"},
 	},
   event = {id = defines.events.on_gui_click, handler = "on_toggle_button_click"},
