ac0225d41fa5d6d0343daec6dc2f5f8811f69c0e
eduran84 <22216687+eduran84@users.noreply.github.com>
2019-04-16 08:30:48 +0200

    Added shortcut icon

--- data.lua
+++ data.lua
@@ -8,3 +8,45 @@ data:extend({{
 	key_sequence = "CONTROL + SHIFT + E",
 	consuming = "none",
 }})
+
+data:extend({{
+  type = "shortcut",
+  name = "ltnt-toggle-shortcut",
+  --order = "b[blueprints]-g[deconstruction-planner]",
+  action = "lua",
+  toggleable = true
+  localised_name = {"shortcut.ltnt-toggle"},
+  style = "default",
+  associated_control_input = "ltnt-toggle-hotkey"
+  icon =
+  {
+    filename = "__LTN_Tracker__/graphics/shortcut_icon_32.png",
+    priority = "extra-high-no-scale",
+    size = 32,
+    scale = 1,
+    flags = {"icon"}
+  },
+  disabled_icon =
+  {
+    filename = "__LTN_Tracker__/graphics/shortcut_icon_32_white.png",
+    priority = "extra-high-no-scale",
+    size = 32,
+    scale = 1,
+    flags = {"icon"}
+  },
+  small_icon =
+  {
+    filename = "__LTN_Tracker__/graphics/shortcut_icon_24.png",
+    size = 24,
+    scale = 1,
+    flags = {"icon"}
+  },
+  disabled_small_icon =
+  {
+    filename = "__LTN_Tracker__/graphics/shortcut_icon_24_white.png",
+    priority = "extra-high-no-scale",
+    size = 24,
+    scale = 1,
+    flags = {"icon"}
+  },
+}})
